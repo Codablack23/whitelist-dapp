6ba58d738f4d4b3f008416f864df4492284ca03e
Codablack <goodluckedih@yahoo.com>
2023-02-05 12:01:27 +0100

    created whitelisted Dapp

---
+++ hardhat-tutorial/hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import {config as dotEnvConfig} from 'dotenv'
+
+dotEnvConfig({
+  path:".env"
+})
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks:{
+    goerli:{
+      url:QUICKNODE_HTTP_URL,
+      accounts:[PRIVATE_KEY as string]
+    }
+  }
+};
+
+export default config;
